69fa68d7c36c86c930f26c9e3f2eb2ff382b36d2
der-den <denny1977@googlemail.com>
2020-07-01 00:05:51 +0200

    Removed weather and random data
    - the weater api is broken, so you would never see data
    - weather Js and HTML is removed
    - the random data block is removed too
    - smaller ASCII ART startpage logo

--- src/js/main.js
+++ src/js/main.js
@@ -115,73 +115,6 @@ function loadJSON() {
     });
 }
 
-// Weather support via simpleWeather v3.1.0 - http://simpleweatherjs.com
-// Replace 'Toronto' with your city name
-function initLocation() {
-    //console.log("Getting weather info...");
-    if ("geolocation" in navigator) {
-        var options = {
-            enableHighAccuracy: false,
-            timeout: 5000,
-            maximumAge: 0
-        };
-
-        function success(pos) {
-            var crd = pos.coords;
-
-            console.log('Your current position is:');
-            console.log('Latitude : ' + crd.latitude);
-            console.log('Longitude: ' + crd.longitude);
-            console.log('More or less ' + crd.accuracy + ' meters.');
-
-            loadWeather(pos.coords.latitude + ',' + pos.coords.longitude);
-        };
-
-        function error(err) {
-            console.warn('ERROR(' + err.code + '): ' + err.message);
-        };
-
-        navigator.geolocation.getCurrentPosition(success, error, options);
-    } else {
-        loadWeather('Toronto', ''); //@params location, woeid
-    }
-}
-
-function loadWeather(location, woeid) {
-    function update() {
-        $('#clock').html(moment().format('MM/DD/YY h:mm:ss a'));
-    }
-    $.simpleWeather({
-        location: location,
-        woeid: woeid,
-        unit: 'c',
-        success: function (weather) {
-            html = '<div class="weather-details"><ul class="weather-current"><li><a href="' + weather.link + '" target="_blank"><i class="icon-weather icon-' + weather.code + '"></i></a> ' + weather.temp + '&deg;' + weather.units.temp + '</li>';
-            html += '<li class="currently">' + weather.currently + '</li>';
-            html += '<li>H:' + weather.high + '&deg;C // L:' + weather.low + '&deg;C</li>';
-            html += '<li>' + weather.city + ', ' + weather.region + '</li></ul>';
-            html += '<ul class="weather-forecast"><li><i class="icon-weather icon-' + weather.forecast[1].code + '"></i><span>' + weather.forecast[1].day + ': ' + weather.forecast[1].text + '<br>H: ' + weather.forecast[1].high + '&deg;C // L: ' + weather.forecast[1].low + '&deg;C</span></li><li><i class="icon-weather icon-' + weather.forecast[2].code + '"></i><span>' + weather.forecast[2].day + ': ' + weather.forecast[2].text + '<br>H: ' + weather.forecast[2].high + '&deg;C // L: ' + weather.forecast[2].low + '&deg;C</span></li><li><i class="icon-weather icon-' + weather.forecast[3].code + '"></i><span>' + weather.forecast[3].day + ': ' + weather.forecast[3].text + '<br>H: ' + weather.forecast[3].high + '&deg;C // L: ' + weather.forecast[3].low + '&deg;C</span></li></ul></div>';
-            html += '<p><span id="clock"></span> // Weather updated ' + moment(weather.updated).fromNow() + '</p>';
-            $("#weather").html(html);
-            //console.log(html);
-            setInterval(update, 1000);
-        },
-        error: function (error) {
-            $("#weather").html('<p>' + error + '</p>');
-        }
-    });
-}
-
-// Totally pointless, yet somewhat stylish extra 1KB of randomly generated pseudo-encryption bloat
-function randomgen() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@^$_@`";
-
-    for( var i=0; i < 1024; i++ )
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    $(".random").text(text+"^^EXTRA1KBPAGELOADWHYNOT?");
-}
 
 // Set a cookie to store prefs
 function createCookie(name,value,days) {
@@ -225,12 +158,8 @@ function readPrefs() {
 
 $(document).ready(function () {
     readPrefs(); // Read site preferences (flicker, colour, etc.)
-    initLocation(); // Request location for weather via the browser
     loadJSON(); // Read the user's links collection
-    randomgen(); // Add 1KB to the page for that A E S T H E T I C look
-    setInterval(randomgen, 300000); // Regenerate the fake encrypted string in footer every 5 minutes because why not?
-    setInterval(initLocation, 600000); // Update the weather every 10 minutes.
-
+ 
     $(".surround").on('click', togglePower);
     $(".power-label").on('click',toggleScanlines);
     $(".theme-button").on('click', toggleTheme);
